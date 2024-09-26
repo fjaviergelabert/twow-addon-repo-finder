eaf56c24cfac96fecc1998d7196b109750598e4a
Kiko <fjavier.gelabert@gmail.com>
2024-09-25 17:09:07 -0700

    Remove unnecessary div

--- components/github-repo-search.tsx
+++ components/github-repo-search.tsx
@@ -19,47 +19,43 @@ export function GithubRepoSearch() {
   );
 
   return (
-    <div className="container mx-auto p-4">
-      <div className="flex flex-col gap-4 items-center md:flex-row justify-center">
-        <div
-          className={`w-full md:w-1/3 ${
-            searchResults!.length > 0 ? "hidden md:block" : "block"
-          }`}
-        >
-          <DirectoryExplorer
-            directories={directories}
-            onDirectorySelect={setDirectories}
-          />
-        </div>
-
-        {directories.length > 0 && (
-          <>
-            <div
-              className={`flex items-center ${
-                searchResults!.length > 0 ? "hidden" : "block"
-              } md:flex`}
-            >
-              <Button
-                onClick={() => searchFetch(directories)}
-                disabled={isLoading || searchResults!.length > 0}
-              >
-                {isLoading ? "Searching..." : "Search GitHub"}
-              </Button>
-            </div>
-
-            <div
-              className={`w-full md:w-1/2 ${
-                searchResults!.length === 0 ? "hidden md:block" : "block"
-              }`}
-            >
-              <h2 className="text-lg font-semibold mb-2">
-                Github download URLs
-              </h2>
-              <ResultsTable searchResults={searchResults!} />
-            </div>
-          </>
-        )}
+    <div className="container mx-auto p-4 flex flex-col gap-4 items-center md:flex-row justify-center">
+      <div
+        className={`w-full md:w-1/3 self-start ${
+          searchResults!.length > 0 ? "hidden md:block" : "block"
+        }`}
+      >
+        <DirectoryExplorer
+          directories={directories}
+          onDirectorySelect={setDirectories}
+        />
       </div>
+
+      {directories.length > 0 && (
+        <>
+          <div
+            className={`flex items-center ${
+              searchResults!.length > 0 ? "hidden" : "block"
+            } md:flex`}
+          >
+            <Button
+              onClick={() => searchFetch(directories)}
+              disabled={isLoading || searchResults!.length > 0}
+            >
+              {isLoading ? "Searching..." : "Search GitHub"}
+            </Button>
+          </div>
+
+          <div
+            className={`w-full md:w-1/2 self-start ${
+              searchResults!.length === 0 ? "hidden md:block" : "block"
+            }`}
+          >
+            <h2 className="text-lg font-semibold mb-2">Github download URLs</h2>
+            <ResultsTable searchResults={searchResults!} />
+          </div>
+        </>
+      )}
     </div>
   );
 }
